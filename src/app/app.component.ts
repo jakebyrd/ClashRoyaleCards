e6f744b6c06e8fcaacdcb10bb3dae18bc0f9a6e8
Jake L. Byrd <jakelbyrd@WWPROGRAMMING.COM>
2018-05-29 15:58:23 -0500

    added functions to get cards with same elixir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent {
 
   readonly cardURL = 'http://www.clashapi.xyz/api/cards'
   cardList: any;
   card: any;
-  elixirCount = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  elixirCount = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  elixir: any;
+
   constructor(private http: HttpClient){
+  }
+
+  form;
+  ngOnInIt(){
+    this.form = new FormGroup({
+      retailer: new FormControl(),
+      elixir: new FormControl()    
+   })
   }
 
   getCards(){
@@ -27,17 +38,17 @@ export class AppComponent{
       this.cardList = card;
       console.log(this.cardList);
     });
-    }
-    // getRequestedElixir(){
+    this.getCardData(this.elixir)
+  }
 
-    // }
-    // getCardData(){
-    // this.cardList.forEach(function(value){
-    //     console.log(value);
-    //   }};
-
-    afterSubmit(elixir){
-
-    }
-  title = 'app';
+  getCardData(elixir){
+    this.cardList.forEach(function(card){
+    if(card.elixirCost == elixir)
+      console.log(card);
+    });
+  }
+  onSubmit(){
+    this.getCards()
+    this.getCardData(this.elixir)
+  }
 }
