6b06066a26dba91a9b7d4a4b568ae02cfdc84fdd
Jake L. Byrd <jake.byrd@wwinc.com>
2018-06-14 10:21:50 -0500

    added meta tags

--- dist/api-app/styles.js
+++ dist/api-app/styles.js
@@ -48,7 +48,7 @@ module.exports = "/*!\n * Bootstrap v4.1.1 (https://getbootstrap.com/)\n * Copyr
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "/* You can add global styles to this file, and also import other style files */\nbody{\n    align-content: center;\n}"
+module.exports = "/* You can add global styles to this file, and also import other style files */\r\nbody{\r\n    align-content: center;\r\n}"
 
 /***/ }),
 
